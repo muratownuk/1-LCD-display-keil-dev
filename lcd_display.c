cebb0e9050021d3e89fef2878b573643110d8df8
Murat Ownuk <ilmuratownuk@gmail.com>
2025-06-03 11:04:07 -0400

    coded template of main lcd file

---
+++ lcd_display.c
@@ -0,0 +1,25 @@
+/*
+    lcd_display: 
+    this is the main module where the program starts. the C8051F005 oscillator 
+    frequency is default (2MHz). 
+*/ 
+/*
+    includes 
+*/
+#include "./misc/device-files/si_toolchain.h" 
+#include "./misc/device-files/c8051F000_defs.h" 
+#include "on_off.h" 
+#include "C8051F005_Init.h" 
+#include "C8051F005_Routines.h" 
+#include "lcd.h" 
+
+
+
+
+
+
+void main(void){
+
+
+
+}
